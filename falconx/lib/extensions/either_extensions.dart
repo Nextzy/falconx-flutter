cb634968baaadfbac4a8431f389c1954c62be2ab
Nonthawit Doungsodsri <nonthawit.kub@gmail.com>
2024-12-07 19:10:27 +0700

    [+] update 3.0.5

--- falconx/lib/extensions/either_extensions.dart
+++ falconx/lib/extensions/either_extensions.dart
@@ -3,6 +3,34 @@ import 'package:falconx/lib.dart';
 extension FalconEitherExtensions<F extends Failure, DATA> on Either<F, DATA> {
   B resolve<B>(B Function(DATA data) data, B Function(Failure fail) fail) =>
       fold(fail, data);
+
+  bool get isFailure => this is Left;
+
+  bool get isData => this is Right;
+
+  DATA get data => fold(
+        (l) {
+          throw Exception('Either is fail.');
+        },
+        (r) => r,
+      );
+
+  DATA? get dataOrNull => fold(
+        (l) => null,
+        (r) => r,
+      );
+
+  Failure get failure => fold(
+        (l) => l,
+        (r) {
+          throw Exception('Either has data not fail.');
+        },
+      );
+
+  Failure? get failureOrNull => fold(
+        (l) => l,
+        (r) => null,
+      );
 }
 
 extension FalconEitherFutureExtensions<F extends Failure, DATA>
--- faltool/lib/faltool.dart
+++ faltool/lib/faltool.dart
@@ -37,3 +37,4 @@ export 'package:path_provider/path_provider.dart';
 export 'package:share_plus/share_plus.dart';
 export 'package:freezed_annotation/freezed_annotation.dart';
 export 'package:json_annotation/json_annotation.dart';
+export 'package:numeral/numeral.dart';
