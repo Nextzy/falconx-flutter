e3a562030854ae52e900780343648992362bbc1a
Nonthawit Doungsodsri <nonthawit.kub@gmail.com>
2024-12-11 09:38:32 +0700

    [+] update 3.0.10

--- falkit/lib/falkit.dart
+++ falkit/lib/falkit.dart
@@ -3,7 +3,7 @@ library;
 export 'package:gap/gap.dart';
 export 'package:flutter_svg/flutter_svg.dart';
 export 'package:smooth_page_indicator/smooth_page_indicator.dart'
-    hide SlideType, ScaleEffect, SlideEffect, SwapEffect;
+    hide SlideType, ScaleEffect, SlideEffect, SwapEffect, ColorBuilder;
 export 'package:extended_image/extended_image.dart'
     hide MultipartFile, BaseRequest, SlideType;
 export 'package:flutter_animate/flutter_animate.dart'
