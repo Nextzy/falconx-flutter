b3a7ee7507813782a957d3a334e2146ef1832593
Nonthawit Doungsodsri <nonthawit.kub@gmail.com>
2024-12-14 19:57:53 +0700

    [+] update extension

--- falconx/lib/extensions/stream_extensions.dart
+++ falconx/lib/extensions/stream_extensions.dart
@@ -12,10 +12,40 @@ extension FalconStreamDataAndFailureExtensions<F extends Failure, DATA>
           (fail) => Left(mapFailure?.call(fail) ?? fail),
         ),
       );
+
+  Stream<Either<F, DATA>> mapEitherFailure<S>(
+    F Function(F failue) mapFailure,
+  ) =>
+      map(
+        (event) => event.resolve(
+          (data) => Right(data),
+          (fail) => Left(mapFailure(fail)),
+        ),
+      );
 }
 
 extension FalconStreamDataAndExceptionExtensions<F extends Failure,
     E extends Exception, DATA> on Stream<Either<E, DATA>> {
+  Stream<Either<E, S>> mapEitherData<S>(
+    S Function(DATA data) mapData,
+  ) =>
+      map(
+        (event) => event.resolve(
+          (data) => Right(mapData(data)),
+          (exception) => Left(exception),
+        ),
+      );
+
+  Stream<Either<F, DATA>> mapEitherException(
+    F Function(E exception) mapException,
+  ) =>
+      map(
+        (event) => event.resolve(
+          (data) => Right(data),
+          (exception) => Left(mapException(exception)),
+        ),
+      );
+
   Stream<Either<F, S>> mapEitherWithFailure<S>(
     S Function(DATA data) mapData,
     F Function(E exception) mapException,
