c5039d23b90a8e528372d9dc39b5ab09708b6fd4
Nonthawit Doungsodsri <nonthawit.kub@gmail.com>
2025-02-22 18:40:42 +0700

    [+] update FalconBlocConsumer

--- falconx/lib/views/states/bloc_consumer.dart
+++ falconx/lib/views/states/bloc_consumer.dart
@@ -1,30 +1,67 @@
 import 'package:falconx/lib.dart';
 
-class WidgetStateBlocConsumer<EVENT, B extends StateStreamable<S>, S>
-    extends BlocConsumer<B, S> {
+class WidgetStateBlocConsumer<
+    PAGE_EVENT,
+    B extends StateStreamable<WidgetStateEvent<DATA?>>,
+    DATA> extends BlocConsumer<B, WidgetStateEvent<DATA?>> {
   WidgetStateBlocConsumer({
     super.key,
     super.bloc,
-    BlocWidgetListenerEvent<EVENT>? listenEvent,
-    BlocWidgetListenerState<S>? listenState,
-    BlocBuilderCondition<S>? buildWhen,
+    BlocWidgetListenerEvent<PAGE_EVENT>? listenEvent,
+    BlocWidgetListenerState<WidgetStateEvent<DATA?>>? listenState,
+    BlocBuilderCondition<WidgetStateEvent<DATA?>>? buildWhen,
     required super.builder,
   }) : super(
           listenWhen: (previous, current) => true,
           listener: (context, state) {
-            if (state is WidgetStateEvent && state.event != null) {
+            if (state.event != null) {
               listenEvent?.call(
-                  context, state.event!.name as EVENT, state.event!.data);
+                  context, state.event!.name as PAGE_EVENT, state.event!.data);
             }
             listenState?.call(context, state);
           },
           buildWhen: (previous, current) {
-            if (current is WidgetStateEvent && current.event != null) {
+            if (current.event != null) {
               // No build the widget when state has event
               return false;
             } else if (buildWhen != null) {
               return buildWhen(previous, current);
-            } else if (current is WidgetStateEvent && current.event == null) {
+            } else if (current.event == null) {
+              return current.build;
+            } else {
+              return true;
+            }
+          },
+        );
+}
+
+class WidgetStateSafeBlocConsumer<
+    PAGE_EVENT,
+    B extends StateStreamable<WidgetStateEvent<DATA>>,
+    DATA> extends BlocConsumer<B, WidgetStateEvent<DATA>> {
+  WidgetStateSafeBlocConsumer({
+    super.key,
+    super.bloc,
+    BlocWidgetListenerEvent<PAGE_EVENT>? listenEvent,
+    BlocWidgetListenerState<WidgetStateEvent<DATA>>? listenState,
+    BlocBuilderCondition<WidgetStateEvent<DATA>>? buildWhen,
+    required super.builder,
+  }) : super(
+          listenWhen: (previous, current) => true,
+          listener: (context, state) {
+            if (state.event != null) {
+              listenEvent?.call(
+                  context, state.event!.name as PAGE_EVENT, state.event!.data);
+            }
+            listenState?.call(context, state);
+          },
+          buildWhen: (previous, current) {
+            if (current.event != null) {
+              // No build the widget when state has event
+              return false;
+            } else if (buildWhen != null) {
+              return buildWhen(previous, current);
+            } else if (current.event == null) {
               return current.build;
             } else {
               return true;
